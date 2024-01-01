90f7ba08c0a8f6cd3a6af3bfb732857bc3672f33
Dasun Tharanga <hello@dasun.dev>
2024-01-01 23:49:29 +0530

    Update breadcrumb

--- fines/edit.php
+++ fines/edit.php
@@ -21,7 +21,7 @@ require_once "process.php";
     <nav class="d-flex align-items-center justify-content-between" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="/fines">Fines</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Assign Fine</li>
+            <li class="breadcrumb-item active" aria-current="page">Edit Fine</li>
         </ol>
 
         <a href="/fines" class="btn btn-primary d-inline-flex align-self-start align-items-center justify-content-between gap-2">
