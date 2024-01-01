3a6755598451d0658d36801e115b3fd4434b363d
Dasun Tharanga <hello@dasun.dev>
2024-01-01 23:05:29 +0530

    Update go back button position

--- fines/create.php
+++ fines/create.php
@@ -18,21 +18,22 @@ require_once "process.php";
 <div class="container-lg mt-4">
     <?php include './../shared/alert.php'; ?>
 
-    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+    <nav class="d-flex align-items-center justify-content-between" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="/fines">Fines</a></li>
             <li class="breadcrumb-item active" aria-current="page">Assign Fine</li>
         </ol>
-    </nav>
 
-    <div class="mb-3 d-flex justify-content-between">
-        <h1 class="fs-2">Assign Fine</h1>
         <a href="/fines" class="btn btn-primary d-inline-flex align-self-start align-items-center justify-content-between gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20" height="20">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
             </svg>
             Go Back
         </a>
+    </nav>
+
+    <div class="mb-3">
+        <h1 class="fs-2">Assign Fine</h1>
     </div>
 
     <form action="process.php" method="post">
