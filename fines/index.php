0af1a23776ad0d21f7cb8eb952ce7fc56557505f
Dasun Tharanga <hello@dasun.dev>
2024-01-01 23:03:07 +0530

    Update button name to assign fine

--- fines/index.php
+++ fines/index.php
@@ -24,7 +24,7 @@ require_once "process.php";
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20" height="20">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
-            Add Fine
+            Assign Fine
         </a>
     </div>
 
