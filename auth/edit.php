35c8f1d6af9ee0422f68191731af4f0f5617cadd
Surekha Sulakshani <sulaksh-ct19052@stu.kln.ac.lk>
2024-01-03 21:07:32 +0530

    Merge with fine.

--- auth/edit.php
+++ auth/edit.php
@@ -6,7 +6,7 @@ require_once "process.php";
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Edit book</title>
+    <title>Edit Fine</title>
 
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -20,11 +20,11 @@ require_once "process.php";
 
     <nav class="d-flex align-items-center justify-content-between" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="/books">Books</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Edit Book</li>
+            <li class="breadcrumb-item"><a href="/fines">Fines</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Edit Fine</li>
         </ol>
 
-        <a href="/books" class="btn btn-primary d-inline-flex align-self-start align-items-center justify-content-between gap-2">
+        <a href="/fines" class="btn btn-primary d-inline-flex align-self-start align-items-center justify-content-between gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20" height="20">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
             </svg>
@@ -33,40 +33,40 @@ require_once "process.php";
     </nav>
 
     <div class="mb-3">
-        <h1 class="fs-2">Edit Book</h1>
+        <h1 class="fs-2">Edit Fine</h1>
     </div>
 
     <?php
-    $bookId = $_GET['bookId'];
+    $fineId = $_GET['fineId'];
 
-    $sql = "SELECT * FROM book WHERE book_id = '$bookId'";
+    $sql = "SELECT * FROM fine WHERE fine_id = '$fineId'";
 
     $result = $database->query($sql) or die($database->error);
 
     $row = $result->fetch_row();
-
-    $sql = "SELECT * FROM bookcategory";
-
-    $categoryResult = $database->query($sql) or die($database->error);
     ?>
 
-    <form action="process.php?update=true&bookId=<?= $row[0] ?>" method="post">
+    <form action="process.php?update=true&fineId=<?= $row[0] ?>" method="post">
         <div class="row">
             <div class="col-lg-5 mb-3">
+                <label for="fineId" class="form-label">Fine ID</label>
+                <input id="fineId" name="fine_id" type="text" class="form-control" value="<?= $row[0] ?>" readonly required>
+            </div>
+            <div class="col-lg-5 mb-3">
+                <label for="memberId" class="form-label">Member ID</label>
+                <input id="memberId" name="member_id" type="text" class="form-control" value="<?= $row[2] ?>" required>
+            </div>
+            <div class="col-lg-5 mb-3">
                 <label for="bookId" class="form-label">Book ID</label>
-                <input id="bookId" name="book_id" type="text" class="form-control" value="<?= $row[0] ?>" pattern="B\d{3}" readonly>
+                <input id="bookId" name="book_id" type="text" class="form-control" value="<?= $row[1] ?>" required>
             </div>
             <div class="col-lg-5 mb-3">
-                <label for="bookName" class="form-label">Book Name</label>
-                <input id="bookName" name="book_name" type="text" class="form-control" value="<?= $row[1] ?>" required>
+                <label for="amount" class="form-label">Fine Amount (Rs)</label>
+                <input id="amount" name="amount" type="number" min="2" max="500" class="form-control" value="<?= $row[3] ?>" required>
             </div>
             <div class="col-lg-5 mb-3">
-                <label for="bookCategory" class="form-label">Book Category</label>
-                <select id="bookCategory" name="category_id" type="text" class="form-control" required>
-                    <?php while($categoryRow = $categoryResult->fetch_assoc()) { ?>
-                        <option value="<?= $categoryRow['category_id'] ?>" <?= $categoryRow['category_id'] == $row[2] ? 'selected' : null ?>><?= $categoryRow['category_Name'] ?></option>
-                    <?php } ?>
-                </select>
+                <label for="date" class="form-label">Date</label>
+                <input id="date" name="date" type="datetime-local" class="form-control" value="<?= $row[4] ?>" required>
             </div>
         </div>
         <button type="submit" class="btn btn-primary">Save</button>
--- auth/process.php
+++ auth/process.php
@@ -4,59 +4,63 @@ require_once "./../config.php";
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_GET['create'])) {
-    $bookId = $_POST['book_id'];
-    $bookName = $_POST['book_name'];
-    $categoryId = $_POST['category_id'];
+    $fineID = $_POST['fine_id'];
+    $memberID = $_POST['member_id'];
+    $bookID = $_POST['book_id'];
+    $fineAmount = $_POST['amount'];
+    $date = $_POST['date'];
 
-    $sql = "INSERT INTO book (book_id, book_name, category_id) VALUES ('$bookId', '$bookName', '$categoryId')";
+    $sql = "INSERT INTO fine (fine_id, member_id, book_id, fine_amount, fine_date_modified) VALUES ('$fineID', '$memberID', '$bookID', '$fineAmount', '$date')";
 
     try {
         $database->query($sql);
 
-        $_SESSION['message'] = "Book added successfully.";
+        $_SESSION['message'] = "Fine added successfully.";
         $_SESSION['message_type'] = "success";
     } catch (Exception $e) {
         $_SESSION['message'] = $e->getMessage();
         $_SESSION['message_type'] = "danger";
     }
 
-    header("Location: /books");
+    header("Location: /fines");
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_GET['update'])) {
-    $bookId = $_POST['book_id'];
-    $bookName = $_POST['book_name'];
-    $categoryId = $_POST['category_id'];
+    $fineID = $_POST['fine_id'];
+    $memberID = $_POST['member_id'];
+    $bookID = $_POST['book_id'];
+    $fineAmount = $_POST['amount'];
+    $date = $_POST['date'];
 
-    $sql = "UPDATE book SET book_name = '$bookName', category_id = '$categoryId' WHERE book_id = '$bookId'";
+    $sql = "UPDATE fine SET member_id = '$memberID', book_id = '$bookID', fine_amount = '$fineAmount', fine_date_modified = '$date' WHERE fine_id = '$fineID'";
 
     try {
         $database->query($sql);
 
-        $_SESSION['message'] = "Book updated successfully.";
+        $_SESSION['message'] = "Fine updated successfully.";
         $_SESSION['message_type'] = "success";
     } catch (Exception $e) {
         $_SESSION['message'] = $e->getMessage();
         $_SESSION['message_type'] = "danger";
     }
 
-    header("Location: /books");
+    header("Location: /fines");
 }
 
 if (isset($_GET['delete'])) {
-    $bookId = $_GET['bookId'];
+    $fineID = $_GET['fineId'];
 
-    $sql = "DELETE FROM book WHERE book_id = '$bookId'";
+    $sql = "DELETE FROM fine WHERE fine_id = '$fineID'";
 
     try {
         $database->query($sql);
 
-        $_SESSION['message'] = "Book deleted successfully.";
+        $_SESSION['message'] = "Fine deleted successfully.";
         $_SESSION['message_type'] = "success";
     } catch (Exception $e) {
         $_SESSION['message'] = $e->getMessage();
         $_SESSION['message_type'] = "danger";
     }
 
-    header("Location: /books");
+    header("Location: /fines");
 }
