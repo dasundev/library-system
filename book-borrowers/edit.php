a5e0a50070b0d78877f7f16ed95e7ff816c7b322
Gimhan <weerasu-ct19027@stu.kln.ac.lk>
2024-01-06 20:13:59 +0530

    fixed the issue of edit.php

--- book-borrowers/edit.php
+++ book-borrowers/edit.php
@@ -20,7 +20,7 @@ require_once "process.php";
 
     <nav class="d-flex align-items-center justify-content-between" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="/books">Books</a></li>
+            <li class="breadcrumb-item"><a href="/book-borrowers">Borrowed Books</a></li>
             <li class="breadcrumb-item active" aria-current="page">Edit Borrower-book</li>
         </ol>
 
