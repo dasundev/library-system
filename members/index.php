bd7a30f09c3f4eb7b9d9cf5c53d4cc98fb0f2e75
NaweedAhamed <147893963+NaweedAhamed@users.noreply.github.com>
2024-02-13 22:54:12 +0530

    index file
    added index file of member registration.

---
+++ members/index.php
@@ -0,0 +1,92 @@
+<?php
+require_once "process.php";
+
+// Check user session exists
+if (! isset($_SESSION['username'])) {
+    header('Location: /auth/login.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>members</title>
+
+    <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<?php include './../shared/navbar.php'; ?>
+
+<div class="container-lg mt-4">
+    <?php include './../shared/alert.php'; ?>
+
+    <div class="mb-3 d-flex justify-content-between">
+        <h1 class="fs-2">Member Registration</h1>
+        <a href="/members/create.php" type="button" class="btn btn-primary d-inline-flex align-self-start align-items-center justify-content-between gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20" height="20">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+            Add Member
+        </a>
+    </div>
+
+    <?php
+    $sql = "SELECT * FROM member ";
+
+    $result = $database->query($sql) or die($database->error);
+    ?>
+
+    <table class="table">
+        <thead class="table-light">
+            <tr>
+                <th>Member ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Birthday</th>
+                <th>Email</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php if ($result->num_rows > 0) { ?>
+            <?php while ($row = $result->fetch_assoc()) { ?>
+                <tr>
+                    <td><?= $row['member_id'] ?></td>
+                    <td><?= $row['first_name'] ?></td>
+                    <td><?= $row['last_name'] ?></td>
+                    <td><?= $row['birthday'] ?></td>
+                    <td><?= $row['email'] ?></td>
+            
+                     
+                    <td>
+                        <a href="/members/edit.php?MemberID=<?= $row['member_id'] ?>" class="btn btn-sm btn-warning">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20" height="20">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                            </svg>
+                        </a>
+                        <a onclick="return confirm('Are you sure?')" href="/members/process.php?delete=true&member_id=<?= $row['member_id'] ?>" class="btn btn-sm btn-danger">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20" height="20">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
+                        </a>
+                    </td>
+                </tr>
+            <?php } ?>
+        <?php } else { ?>
+            <tr>
+                <td colspan="7" class="text-center text-secondary">No records are available!</td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+</div>
+
+<!-- Scripts -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
